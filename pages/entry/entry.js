ba4304b39b10fb614a6782a629b92736c40c083d
雷小天博客 <tian-lei-happy@163.com>
2017-09-08 14:10:16 +0800

    pages资源

---
+++ pages/entry/entry.js
@@ -0,0 +1,80 @@
+// entry.js
+var WxParse = require('../../wxParse/wxParse.js');
+
+const toolbar = [
+  '../../images/nav/download.png', '../../images/nav/fav.png',
+  '../../images/nav/share.png', '../../images/nav/comment.png',
+];
+const app = getApp();
+
+Page({
+  data: {
+    // 当前日志详情
+    diary: undefined,
+
+    // 右上角工具栏
+    toolbar: toolbar,
+
+    // 图片预览模式
+    previewMode: false,
+
+    // 当前预览索引
+    previewIndex: 0,
+
+    // 多媒体内容列表
+    mediaList: [],
+  },
+
+  // 加载日记
+  getDiary(params) {
+    console.log("Loading diary data...", params);
+
+    var id = params["id"], diary;
+    
+
+    this.setData({
+      diary: diary,
+    });
+  },
+  getDetail(id) {
+    var that = this
+    wx.request({
+      url: 'https://www.100txy.com/weixin/getdowndetail?id='+id, //仅为示例，并非真实的接口地址
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+
+        console.log(res.data)
+        var article = res.data.content;
+        // var that = this;
+        WxParse.wxParse('article', 'html', article, that, 5);
+        that.setData({
+          Downdetail: res.data
+        })
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+  },
+  
+  
+
+
+  onLoad: function(params) {
+    var id=params.id;
+    console.log(params);
+    console.log(params.id);
+    // this.getDiary(params);
+    // this.getMediaList();
+    this.getDetail(id);
+    // var article = '<div>我是HTML代码</div>';
+    // var that = this;
+    // WxParse.wxParse('article', 'html', article, that, 5);
+  },
+
+  onHide: function() {
+  },
+})
---
+++ pages/news/news.js
@@ -0,0 +1,110 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    newsTitles:['首页','说说','分类','相册','下载','留言','关于'],
+    bannerUrls:[
+      '/images/01.jpg',
+      '/images/02.jpg',
+      '/images/03.jpg',
+    ],
+    news:[
+      {title:'素材1',imgUrl:'/images/1.jpg'},
+      { title: '素材2', imgUrl: '/images/2.jpg' },
+      { title: '素材3', imgUrl: '/images/3.jpg' },
+      { title: '素材4', imgUrl: '/images/4.jpg' },
+      { title: '素材5', imgUrl: '/images/5.jpg' },
+      { title: '素材6', imgUrl: '/images/6.jpg' },
+    ],
+    newsList:[
+      {title_1:'微信小程序开发教程',title_2:'微信小程序开发实际案例分享-雷小天博客',imgUrl:'/images/2.jpg',count:'1256'},
+      { title_1: '微信小程序开发教程', title_2: '微信小程序开发实际案例分享-雷小天博客', imgUrl: '/images/3.jpg', count: '1256' },
+      { title_1: '微信小程序开发教程', title_2: '微信小程序开发实际案例分享-雷小天博客', imgUrl: '/images/4.jpg', count: '1256' },
+      { title_1: '微信小程序开发教程', title_2: '微信小程序开发实际案例分享-雷小天博客', imgUrl: '/images/5.jpg', count: '1256' },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    wx.request({
+      url: 'https://www.100txy.com/weixin/getarticle', //仅为示例，并非真实的接口地址
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          Articles: res.data
+        })  
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+  // 查看详情
+  artDetail(event) {
+    wx.navigateTo({
+      url: '../article/article?id=' + event.currentTarget.id,
+    });
+  },
+  // 查看详情
+  showDetail(event) {
+    wx.navigateTo({
+      url: '../entry/entry?id=' + event.currentTarget.id,
+    });
+  }
+})
